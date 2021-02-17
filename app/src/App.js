d1671142b09c8a0047cd1fe8073a1a4b37ee5437
Benjamin Vaccaro <benvaccaro14@gmail.com>
2021-02-16 20:32:52 -0500

    inital commit

---
+++ app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './media/sofa.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Cloud Upholstery</h1>
+        <p>
+          A software solutions company, driven to make growing your company. More Comfortable.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
